f27ac86d6ab446ef7701d802f1f3caebf7bfe6eb
Patrick Rodrigues <contact@rodriguespatrick.com>
2020-02-21 09:42:58 +0100

    finish count_th.py

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,15 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+# O(n)
 def count_th(word):
+    count = 0
+    def count_substring_occurences(string):
+        if "th" in string:
+            nonlocal count
+            count += 1
+            newStr = string.replace("th", "00", 1)
+            count_substring_occurences(newStr)
     
-    # TBC
-    
-    pass
+    count_substring_occurences(word)
+    return count
